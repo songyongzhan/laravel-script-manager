7520bf5becc26c7de91f50be0ff1eb67130c97c9
songyz <574482856@qq.com>
2020-05-23 15:19:55 +0800

    更新git文件

--- config/app.php
+++ config/app.php
@@ -174,8 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Songyz\Providers\ScaffoldConfigPublishProvider::class,
-
+        \Songyz\Simple\Orm\Providers\ScaffoldConfigPublishProvider::class
     ],
 
     /*
